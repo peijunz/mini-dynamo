eef79a8d265889d10b605eeec351cd88e83b107e
zlyu39 <zlyu39@gatech.edu>
2019-04-21 20:20:54 -0400

    define classes

---
+++ consistent_hash.hpp
@@ -0,0 +1,11 @@
+#ifndef _NODE_
+#define _NODE_
+
+#include "node.hpp"
+
+// consistent hashing of virtual nodes
+class ConsistentHashing {
+
+};
+
+#endif
---
+++ node.hpp
@@ -0,0 +1,14 @@
+#ifndef _NODE_
+#define _NODE_
+
+// virtual node
+class VirtualNode {
+
+};
+
+// real node
+class RealNode {
+
+};
+
+#endif
